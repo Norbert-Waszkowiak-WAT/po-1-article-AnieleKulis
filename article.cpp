31d6d81a824e32c69b70c6ee9b3188b5b774d287
AnieleKulis <gkulis4@wp.pl>
2024-11-18 10:27:20 +0000

    huuh

---
+++ article.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include "author.cpp"
+class Article{
+    private:
+      std:: string title;
+      Author author;
+      int publicationYear;
+      std:: string journal;
+    public:
+      Article()
+      : title(""),author(Author()),publicationYear(0),journal(""){};
+      Article(std::string articleTitle, Author articleAuthor,int year,std::string journalName)
+      :title(articleTitle), author(articleAuthor), publicationYear(publicationYear),journal(journalName){};
+      Article(Article &other)
+      :title(other.title),author(other.author),publicationYear(other.publicationYear),journal(other.journal){};
+   std::string getJournal(){
+    return journal;
+   };
+Author getAuthor(){
+    return author;
+  };
+  std::string getTitle(){
+    return title;
+  };
+int getPublicationYear(){
+    return publicationYear;
+};
+void displayInfo(){
+    std::cout<<"skibidi mostek";
+};
+};
