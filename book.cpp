f595d5218614c85b7720d472e63ce3117da11884
AnieleKulis <gkulis4@wp.pl>
2024-11-25 09:05:35 +0000

    a

--- book.cpp
+++ book.cpp
@@ -2,25 +2,28 @@
 #define UNTITLED1_BOOK_H
 #include <iostream>
 #include <string>
+#include <vector>
 #include "author.cpp"
 #include "article.cpp"
 #include "chapter.cpp"
+using namespace std;
 class Book{
     private:
-      std:: string title;
+      string title;
       Author author;
       int publicationYear;
-      std::vector <Chapter> chapters;
+      vector <Chapter> chapters;
     public:
     Book()
-    :title(""),author(Author()),publicationYear(2023),chapters(){};
+    :title(""),author(Author()),publicationYear(0),chapters(){};
 
-    Book(std::string bookTitle, Author bookAuthor,int year,std::vector <Chapter>bookChapters)
+    Book(string bookTitle, Author bookAuthor,int year,const vector <Chapter>bookChapters)
     :title(bookTitle),author(bookAuthor),publicationYear(year),chapters(bookChapters){};
-    void addChapter(Chapter &chapters){
+
+    void addChapter(const Chapter &chapter){
       chapters.push_back(chapter);
     }
-    std:: string getTitle(){
+    string getTitle(){
         return title;
     }
     Author getAuthor(){
@@ -29,11 +32,11 @@ class Book{
     int getPublicationYear(){
         return publicationYear;
     }
-    vector <Chapter>Book::getChapters(){
+    vector <Chapter> getChapters(){
       return chapters;
     }
     void displayInfo(){
-  std::cout<<"mostek";
+    cout<<"mostek";
 }
 };
 
